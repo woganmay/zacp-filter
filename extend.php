b93a7f6bb2e0b41aaf3bfc5fdfe66c233302afa2
Wogan May <wogan.may@gmail.com>
2019-03-26 12:47:36 +0200

    Fixing

--- extend.php
+++ extend.php
@@ -8,12 +8,8 @@ use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
-
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
-            $config->Censor->word('nuggets');
+            $config->Censor->add('nuggets');
         })
 ];
